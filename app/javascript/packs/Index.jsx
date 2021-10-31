64d3ebd1d93810719f97b8f5cef4ea242989e404
Muco <mmozcan4@gmail.com>
2021-10-31 19:13:08 +0300

    popperjs bug fix

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
-import Popper from 'popper.js';
+import Popper from '@popperjs/core';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
 
